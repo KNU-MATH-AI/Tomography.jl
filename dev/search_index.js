1b3d13de6d50a0b755093459a57bbe3108330fb7
Documenter.jl <documenter@juliadocs.github.io>
2022-01-11 06:07:03 +0000

    build based on f417388

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Tomography.jl","page":"Home","title":"Tomography.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Tomography.jl","category":"page"},{"location":"#Installation","page":"Home","title":"Installation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"The package is available for Julia versions 1.0 and later. To install it, run","category":"page"},{"location":"","page":"Home","title":"Home","text":"using Pkg\nPkg.add(\"Tomography\")","category":"page"},{"location":"","page":"Home","title":"Home","text":"from the Julia REPL.","category":"page"}]
+}
